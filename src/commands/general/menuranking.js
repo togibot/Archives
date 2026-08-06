7eadb092956bcc39fa33d6896fd574d2dfbf4aa7
togibot <lztogibot@gmail.com>
2026-08-06 09:05:10 -0300

    feat: add ranking submenu

---
+++ src/commands/general/menuranking.js
@@ -0,0 +1,9 @@
+export default {
+  name: 'menuranking',
+  aliases: ['rankingmenu'],
+  category: 'geral',
+  description: 'Menu de rankings',
+  async execute({ reply }) {
+    return reply(`╭━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╮\n┃  🏆 𝚁𝙰𝙽𝙺𝙸𝙽𝙶𝚂 • 𝚃𝙾𝙶𝙸\n╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯\n\n🪙 *ECONOMIA*\n• .rich 🔜\n\n⭐ *PROGRESSO*\n• .rank 🔜\n• .quizrank 🔜\n• .rpgrank 🔜\n• .toppets 🔜\n\n🏆 Compete e suba no ranking!`);
+  }
+};
