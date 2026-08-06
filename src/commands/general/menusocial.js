44bede40ecaec376f39a9c249cdbe3a67a88cc60
togibot <lztogibot@gmail.com>
2026-08-06 09:04:37 -0300

    feat: add social submenu

---
+++ src/commands/general/menusocial.js
@@ -0,0 +1,9 @@
+export default {
+  name: 'menusocial',
+  aliases: ['socialmenu'],
+  category: 'geral',
+  description: 'Menu social',
+  async execute({ reply }) {
+    return reply(`╭━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╮\n┃  💞 𝚂𝙾𝙲𝙸𝙰𝙻 • 𝚃𝙾𝙶𝙸\n╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯\n\n👤 *PERFIL*\n• .perfil\n\n❤️ *SOCIAL*\n• .ship\n\n💍 *EM BREVE*\n• .casal\n• .casamento\n• .divorcio\n\n✨ Mais sistemas sociais chegando!`);
+  }
+};
