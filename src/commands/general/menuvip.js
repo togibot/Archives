d0544bb57eba5ed5af37704f894515aeff2d26fa
togibot <lztogibot@gmail.com>
2026-08-06 09:05:33 -0300

    feat: add vip submenu

---
+++ src/commands/general/menuvip.js
@@ -0,0 +1,9 @@
+export default {
+  name: 'menuvip',
+  aliases: ['vipmenu'],
+  category: 'geral',
+  description: 'Menu VIP',
+  async execute({ reply }) {
+    return reply(`╭━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╮\n┃  👑 𝚅𝙸𝙿 • 𝚃𝙾𝙶𝙸\n╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯\n\n✨ *VIP*\n• .vip 🔜\n• .vipinfo 🔜\n\n🤖 *LO BOT*\n• .LoBot 🔜\n\n💎 Recursos exclusivos para membros VIP.`);
+  }
+};
