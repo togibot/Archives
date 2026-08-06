f9f87f9bbc278fe8dfb15798683472a1d539ec19
togibot <lztogibot@gmail.com>
2026-08-06 09:05:16 -0300

    feat: add events submenu

---
+++ src/commands/general/menueventos.js
@@ -0,0 +1,9 @@
+export default {
+  name: 'menueventos',
+  aliases: ['eventosmenu'],
+  category: 'geral',
+  description: 'Menu de eventos',
+  async execute({ reply }) {
+    return reply(`╭━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╮\n┃  🎁 𝙴𝚅𝙴𝙽𝚃𝙾𝚂 • 𝚃𝙾𝙶𝙸\n╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯\n\n🎉 *EVENTOS*\n• .eventos 🔜\n• .participar 🔜\n• .eventrank 🔜\n\n🏆 *PRÊMIOS*\n🪙 Tokens\n🎁 Itens especiais\n⭐ Recompensas exclusivas\n\n🔥 Novos eventos serão adicionados em breve!`);
+  }
+};
